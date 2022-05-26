e351fc380d43b58738e776e8192a7b8a0edcec53
WilliamDjo <william.djong@yahoo.com>
2022-05-27 00:06:36 +0700

    Changed from new Fracty to fracty in line 137

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -134,7 +134,7 @@ class RecipeView extends View {
         <use href="${icons}.svg#icon-check"></use>
         </svg>
         <div class="recipe__quantity">${
-          ing.quantity ? new Fracty(ing.quantity).toString() : ''
+          ing.quantity ? fracty(ing.quantity).toString() : ''
         }</div>
         <div class="recipe__description">
         <span class="recipe__unit">${ing.unit}</span>
