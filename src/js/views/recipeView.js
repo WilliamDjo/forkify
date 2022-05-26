582e8ab3aa924f346fa09fca5af9ab5fab5420f6
WilliamDjo <william.djong@yahoo.com>
2022-05-27 00:04:00 +0700

    Installed fracty and changed import fraction to fracty in recipeView.js

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,5 @@
 import icons from 'url:../../img/icons.svg'; // Do this for importing any static assets that are not programming files
-import Fractional from 'fractional';
+import Fractional from 'fracty';
 import View from './View';
 
 class RecipeView extends View {
@@ -134,7 +134,7 @@ class RecipeView extends View {
         <use href="${icons}.svg#icon-check"></use>
         </svg>
         <div class="recipe__quantity">${
-          ing.quantity ? new Fraction(ing.quantity).toString() : ''
+          ing.quantity ? new Fracty(ing.quantity).toString() : ''
         }</div>
         <div class="recipe__description">
         <span class="recipe__unit">${ing.unit}</span>
